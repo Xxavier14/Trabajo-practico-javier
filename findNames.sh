b2ace70c9278b953a139befa16fe43250bd24faa
Xxavier14 <juanpablojavier14@gmail.com>
2022-12-14 17:27:28 -0300

    update findNames

--- findNames.sh
+++ findNames.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+
+
+. filtro.sh
+#filtra palabra que comiencen con Mayuscula 
+
+
+texto=$(cat texto.txt)
+regex="^[A-Z]{1}([a-z]+$)"
+
+
+touch filtradas.txt
+filtradas=filtradas.txt
+
+
+for i in $texto
+
+do
+  [[ $(filtro $i) =~ $regex ]] && echo $(filtro $i) >> $filtradas
+  
+done
+
+echo "A continuacion una lista de los nombres propios:" ; cat $filtradas
+rm filtradas.txt
+exit 0
