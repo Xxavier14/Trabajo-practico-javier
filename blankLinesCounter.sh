e478693a6f9b44641d97f8428b58daddb0b9ca92
Xxavier14 <juanpablojavier14@gmail.com>
2022-12-14 17:25:45 -0300

    update blankLinesCounter

--- blankLinesCounter.sh
+++ blankLinesCounter.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+echo "La cantidad de lineas vacías es: $(grep -cx '' texto.txt) "
 
+# la funciona grep busca la conincidencia con el espacio vacio
+# -c devuelve un recuento de las veces que se filtro
+# -x seleciona las coincidencias que cubren exactamente toda la linea
+exit 0
