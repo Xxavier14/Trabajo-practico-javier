6d52eaf7053fc55c4dc2954771dcb49720f74525
Xxavier14 <juanpablojavier14@gmail.com>
2022-12-14 17:52:36 -0300

    update statsWords

--- statsWords.sh
+++ statsWords.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
 
+
+. filtro.sh
+texto=$(cat texto.txt)
+
+#inicializacion de variables
+palabraLarga=""
+palabraCorta=""
+cantidadTotal=0
+longitudTotal=0
+
+for i in $texto
+do
+filtr=$(filtro $i)
+  if [ $cantidadTotal == 0 ]
+  then
+    palabraCorta=$filtr
+    palabraLarga=$filtr
+  else
+    if [ ${#filtr} -gt ${#palabraLarga} ]
+    then
+      palabraLarga=$filtr
+    elif [ ${#filtr} -lt ${#palabraCorta} ]
+    then
+      palabraCorta=$filtr
+    fi
+  fi
+  cantidadTotal=$(($cantidadTotal + 1))
+  longitudTotal=$(($longitudTotal + ${#filtr}))
+done
+echo "La palabra mas larga es: $palabraLarga"
+echo "La palabra mas corta es: $palabraCorta"
+echo "La longitud promedio de las palabras es de $(echo "scale=2; (($longitudTotal/$cantidadTotal))" | bc)"
+# scale=2 para 2 decimales 0.00
